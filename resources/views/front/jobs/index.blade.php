92e144086f405d0edf978c901c758c33e4b153a3
Denis Amah <ronsard20214@gmail.com>
2026-05-06 14:54:50 +0000

    update

--- resources/views/front/jobs/index.blade.php
+++ resources/views/front/jobs/index.blade.php
@@ -22,8 +22,8 @@
                                         </a>
                                     </div>
                                     <div class="blog-post-details">
-                                        <h4 class="blog-title mb-2" style="font-size:0.5rem;">
-                                            <a href="{{ route('jobs.show', ['slug' => $offer->slug]) }}">{{ $offer->title }}</a>
+                                        <h4 class="blog-title mb-2">
+                                            <a href="{{ route('jobs.show', ['slug' => $offer->slug]) }}" style="font-size:0.85rem !important; line-height:1.25 !important;">{{ $offer->title }}</a>
                                         </h4>
                                         @if($offer->published_at)
                                             <div class="blog-post-meta mb-2">
